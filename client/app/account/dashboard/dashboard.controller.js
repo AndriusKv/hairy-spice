84199002ded9c3b2336ddf1f2a410b241c463a5a
atskalunas <atskalun4s@gmail.com>
2015-06-18 15:51:03 +0000

    More work on dashboard

--- client/app/account/dashboard/dashboard.controller.js
+++ client/app/account/dashboard/dashboard.controller.js
@@ -3,6 +3,9 @@
 angular.module('workspaceApp').controller('DashboardCtrl', function ($scope) {
     $scope.showNewPoll = true;
     $scope.showPollManage = false;
+    $scope.pollQuestion = "";
+    $scope.createdPolls = [];
+    $scope.options = [1, 2];
       
     $scope.togglePoolCreation = function() {
         if ($scope.showPollManage) {
@@ -18,5 +21,25 @@ angular.module('workspaceApp').controller('DashboardCtrl', function ($scope) {
         }
         
         $scope.showPollManage = !$scope.showPollManage;
+    };
+    
+    $scope.addOption = function() {
+        var i = $scope.options[$scope.options.length - 1];
+        
+        $scope.options.push(++i);
+    };
+    
+    $scope.createPoll = function(pollQuestion) {
+        var question = pollQuestion;
+        
+        if (question.charAt(question.length - 1) !== "?") {
+            question = question + "?";
+        }
+        
+        $scope.createdPolls.push({question: question});
+    };
+    
+    $scope.deleteCreatedPoll = function(index) {
+        $scope.createdPolls.splice(index, 1);
     };
   });
