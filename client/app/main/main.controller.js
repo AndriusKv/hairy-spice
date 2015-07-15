4b9297407cff02d6781b98257f88558fc54e03f8
Andrius Kvedaras <ikandrius@gmail.com>
2015-07-15 17:45:58 +0000

    Added dashboard, logout buttons to home page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,21 +1,10 @@
 'use strict';
 
-angular.module('workspaceApp').controller('MainCtrl', function ($scope, $http) {
-    // $scope.awesomeThings = [];
+angular.module('workspaceApp').controller('MainCtrl', function ($scope, Auth) {
+    $scope.isLoggedIn = Auth.isLoggedIn;
     
-    // $http.get('/api/things').success(function(awesomeThings) {
-    //   $scope.awesomeThings = awesomeThings;
-    // });
-
-    // $scope.addThing = function() {
-    //   if($scope.newThing === '') {
-    //     return;
-    //   }
-    //   $http.post('/api/things', { name: $scope.newThing });
-    //   $scope.newThing = '';
-    // };
-
-    // $scope.deleteThing = function(thing) {
-    //   $http.delete('/api/things/' + thing._id);
-    // };
+    $scope.logout = function() {
+      Auth.logout();
+    };
+    
  });
