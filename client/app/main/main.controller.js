732fab0a8fef5b4271006a49a70811534eb84830
Andrius Kvedaras <ikandrius@gmail.com>
2015-07-15 17:26:47 +0000

    Added login button to home page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,25 +1,21 @@
 'use strict';
 
-angular.module('workspaceApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    // $routeParams.users = [];
-    // $routeParams.users.push($scope.getCurrentUser().name);
+angular.module('workspaceApp').controller('MainCtrl', function ($scope, $http) {
+    // $scope.awesomeThings = [];
     
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    // $http.get('/api/things').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    // });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
+    // $scope.addThing = function() {
+    //   if($scope.newThing === '') {
+    //     return;
+    //   }
+    //   $http.post('/api/things', { name: $scope.newThing });
+    //   $scope.newThing = '';
+    // };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-  });
+    // $scope.deleteThing = function(thing) {
+    //   $http.delete('/api/things/' + thing._id);
+    // };
+ });
